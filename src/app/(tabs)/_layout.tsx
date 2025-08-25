90707499fb0e8729682d3143e28817013bcd9f98
paro-418 <vermapraween6543@gmail.com>
2025-08-25 21:00:12 +0530

    removed hamburger fromheader

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,8 +1,8 @@
-import { Pressable, StyleSheet, Text, View } from "react-native";
-import React from "react";
-import { Tabs } from "expo-router";
-import MaterialIcons from "@expo/vector-icons/MaterialIcons";
-import { Colors } from "../../../constants/Colors";
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { Tabs } from 'expo-router';
+import MaterialIcons from '@expo/vector-icons/MaterialIcons';
+import { Colors } from '../../../constants/Colors';
 
 const AppLayout = () => {
   return (
@@ -10,57 +10,57 @@ const AppLayout = () => {
       screenOptions={{
         headerTitleStyle: {
           color: Colors.primaryColor,
-          fontWeight: "700",
+          fontWeight: '700',
           fontSize: 24,
-          textAlign: "center",
+          textAlign: 'center',
         },
-        headerRight: () => (
-          <Pressable style={{ marginRight: 10 }}>
-            <MaterialIcons name="menu" size={24} color={Colors.primaryColor} />
-          </Pressable>
-        ),
+        // headerRight: () => (
+        //   <Pressable style={{ marginRight: 10 }}>
+        //     <MaterialIcons name="menu" size={24} color={Colors.primaryColor} />
+        //   </Pressable>
+        // ),
         tabBarActiveTintColor: Colors.primaryColor,
-        tabBarInactiveTintColor: "#fff",
+        tabBarInactiveTintColor: '#fff',
         tabBarStyle: {
           backgroundColor: Colors.primaryColor,
           height: 60,
         },
-        tabBarActiveBackgroundColor: "#fff",
+        tabBarActiveBackgroundColor: '#fff',
       }}
     >
       <Tabs.Screen
-        name="home"
+        name='home'
         options={{
-          title: "Home",
+          title: 'Home',
           tabBarIcon: ({ color }) => (
-            <MaterialIcons name="home" size={24} color={color} />
+            <MaterialIcons name='home' size={24} color={color} />
           ),
         }}
       />
       <Tabs.Screen
-        name="courses"
+        name='courses'
         options={{
-          title: "Courses",
+          title: 'Courses',
           tabBarIcon: ({ color }) => (
-            <MaterialIcons name="book" size={24} color={color} />
+            <MaterialIcons name='book' size={24} color={color} />
           ),
         }}
       />
       <Tabs.Screen
-        name="aboutus"
+        name='aboutus'
         options={{
-          title: "About Us",
+          title: 'About Us',
           tabBarIcon: ({ color }) => (
-            <MaterialIcons name="info" size={24} color={color} />
+            <MaterialIcons name='info' size={24} color={color} />
           ),
         }}
       />
       <Tabs.Screen
-        name="team"
+        name='team'
         options={{
-          title: "Team",
+          title: 'Team',
           tabBarIcon: ({ color }) => (
-            <MaterialIcons name="group" size={24} color={color} />
+            <MaterialIcons name='group' size={24} color={color} />
           ),
         }}
       />
